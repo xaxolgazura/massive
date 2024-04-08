b75b449bf60e1f7361678674752e91bee07294ee
иван <xaxolgazarov@mail.ru>
2024-04-08 22:59:40 +0300

    commit message

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // task № 1 - 2
+        // task № 1
         int[] weight = new int[12];
         double[] second = {1.57, 7.654, 9.986};
         weight[0] = 1;
@@ -21,40 +21,87 @@ public class Main {
             System.out.print(weights[i] + ", ");
         }
         System.out.println();
-        // task № 3
-        int[] weight1 = new int[3];
         {
-            for (int i = weight1.length - 1; i >= 0; i--) {
-                weight1[i] = i + 1;
-                System.out.print(weight1[i] + ", ");
-                if (i < weight1.length - 1) {
-                    System.out.print("");
-                }
+        }
+        int[] weights4 = new int[10];
+        weights4[0] = 60;
+        int januaryWeight = weights4[0];
+        {
+            System.out.print(januaryWeight + "  ");
+            System.out.print(weights4[0]);
+        }
+        System.out.println();
+        {
+        }
+        // task № 2
+        int[] weights5 = new int[3];
+        weights5[0] = 30;
+        weights5[1] = 40;
+        weights5[2] = 50;
+        System.out.print(weights5[0] + "," + weights5[1] + "," + weights5[2]);
+        {
+        }
+        System.out.println();
+        {
+        }
+        int[] weights6 = new int[3];
+        weights6[0] = 60;
+        weights6[1] = 70;
+        weights6[2] = 80;
+        System.out.print(weights6[0] + "," + weights6[1] + "," + weights6[2]);
+        {
+        }
+        System.out.println();
+        {
+        }
+        int[] weights7 = new int[3];
+        weights7[0] = 90;
+        weights7[1] = 100;
+        weights7[2] = 200;
+        System.out.print(weights7[0] + "," + weights7[1] + "," + weights7[2]);
+        {
+        }
+        System.out.println();
+        {
+        }
+        // task № 3
+
+        int[] meanFirst = new int[3];
+        meanFirst[0] = 1;
+        meanFirst[1] = 2;
+        meanFirst[2] = 3;
+        for (int i = 2; i >= 0; i--) {
+            System.out.print(meanFirst[i] + ", ");
+        }
+        System.out.println();
+        double[] meaningSecond = {1.57, 7.654, 9.986};
+        for (int i = 2; (i >= 0); i--) {
+            System.out.print(meaningSecond[i] + ", ");
+            {
             }
-            System.out.println();
-            double[] weight2 = {1.57, 7.654, 9.986};
-            for (int i = weight2.length - 1; i >= 0; i--) {
-                System.out.print(weight2[i] + ", ");
-                if (i < weight2.length - 1) {
-                    System.out.print("");
-                }
+        }
+
+        // task № 4
+        int[] sweet = {1, 2, 3, 4, 5};
+        for (int i = 0; i < sweet.length; i++) {
+            if (sweet[i] % 2 != 0) {
+                sweet[i]++;
+                System.out.println();
             }
-            System.out.println();
-            // task № 4
-            int[] ferst = new int[5];
-            int even = 0;
-            for (int i = 0; i < ferst.length; i++) {
-                if (even % 2 == 0) {
-                     i = i + 1;
-                    System.out.print(i + 1);
-                    System.out.print(" ");
-                    {
-                    }
-                }
+        }
+        System.out.println("Результат преобразования");
+        for (int i = 0; i <= sweet.length; i++) {
+            System.out.print(sweet[i]);
+            if (i != sweet.length - 1) {
+                System.out.print(", ");
             }
         }
     }
-    }
+}
+
+
+
+
 
 
 
